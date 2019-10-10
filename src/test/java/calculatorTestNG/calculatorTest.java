0583350652afcc77e0b356ab7b9363ce274faaf1
user <user@192.168.10.5>
2019-10-10 11:40:38 +0900

    testng in pom,calculatortest2

--- src/test/java/calculatorTestNG/calculatorTest.java
+++ src/test/java/calculatorTestNG/calculatorTest.java
@@ -30,40 +30,9 @@ public class calculatorTest {
 	  
 	  @Parameters({"param1", "param2"})
 	  
-	  @Test(priority=4) public void multiply(int param1,int param2) {
-	  System.out.println("Multiplication result :" +objcal.Multiply(param1,
+	  @Test(priority=4) 
+	  public void multiply(int param1,int param2) {
+	     System.out.println("Multiplication result :" +objcal.Multiply(param1,
 	  param2)); }
-	 
-	
-	
-	@Test(priority=5,dataProvider = "provideNumbers")
-	public void Add1(int num1, int num2) {
-		System.out.println("Addition result :" + objcal.Add(num1, num2));
-		//Assert.assertEquals(num1, num1);
-	}
-	
-	@Test(priority=6,dataProvider = "provideNumbers")
-	  public void subtract1(int num1, int num2) {
-	  System.out.println("Subtraction result :" + objcal.Subtract(num1, num2)); }
-	
-	
-	  @Test(priority=7,dataProvider = "provideNumbers") 
-	  public void divide2(int num1, int num2) { 
-		  System.out.println("Division result :" + objcal.Divide(num1, num2)); }
-	
-
-	  @Test(priority=8,dataProvider = "provideNumbers") 
-	  public void multiply3(int num1, int num2) {
-	  System.out.println("Multiplication result :" +objcal.Multiply(num1, num2)); }
-
-	@DataProvider(name = "provideNumbers")
-	public Object[][] provideData() {
-
-		return new Object[][] { 
-			{ 10, 20 }, 
-			{ 100, 110 }, 
-			{ 200, 210 } 
-		};
-	}
 	 
 }
